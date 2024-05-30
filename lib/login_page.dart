598909b04a3120450ee67f713bbfef5338176e37
Faik-irkham <faikirkham@gmail.com>
2024-05-30 15:51:10 +0700

    update

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously, unused_local_variable
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:led_controll/home_page.dart';
